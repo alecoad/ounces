3d601ed220bf491bce1c8343b85626f682ea0ebc
alecoad <andrewlecoad@gmail.com>
2021-03-02 21:43:01 -0700

    nest functions within eventlistener

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let counter = 0;
 
-function count_up() {
-  counter++;
-  document.querySelector('h2').innerHTML = counter;
-}
-
-function count_down() {
-  counter--;
-  document.querySelector('h2').innerHTML = counter;
-}
-
 function clear_count() {
   counter = 0;
   document.querySelector('h2').innerHTML = counter;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.countup').onclick = function() {
+    counter++;
+    document.querySelector('h2').innerHTML = counter;
+  }
+
+  document.querySelector('.countdown').onclick = function() {
+    counter--;
+    document.querySelector('h2').innerHTML = counter;
+  }
+});
+
