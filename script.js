d21546db6f9cd8018eb444ada55e7df302d0f1a6
alecoad <andrewlecoad@gmail.com>
2021-03-08 21:21:36 -0700

    count buttons use id

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ let counter = 0;
 function clear_count() {
   counter = 0;
   document.querySelector('h2').innerHTML = counter;
-  document.querySelector('.countdown').disabled = true;
+  document.querySelector('#countdown').disabled = true;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.countdown').disabled = true;
+  document.querySelector('#countdown').disabled = true;
 
-  document.querySelector('.countup').onclick = function() {
+  document.querySelector('#countup').onclick = function() {
     counter++;
     document.querySelector('h2').innerHTML = counter;
-    document.querySelector('.countdown').disabled = false;
+    document.querySelector('#countdown').disabled = false;
   }
 
-  document.querySelector('.countdown').onclick = function() {
+  document.querySelector('#countdown').onclick = function() {
     if (counter <= 0) {
-      document.querySelector('.countdown').disabled = true;
+      document.querySelector('#countdown').disabled = true;
     }
     else {
-      document.querySelector('.countdown').disabled = false;
+      document.querySelector('#countdown').disabled = false;
       counter--;
       document.querySelector('h2').innerHTML = counter;
       if (counter == 0) {
-        document.querySelector('.countdown').disabled = true;
+        document.querySelector('#countdown').disabled = true;
       }
     }
   }
